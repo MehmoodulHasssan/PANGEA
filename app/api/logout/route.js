44510b7f44c45e36d7de65958095ed751c76e760
Mehmood Ul Hassan <mehmoodjutt471@gmail.com>
2024-09-04 01:09:18 +0500

    fixed bug

--- app/api/logout/route.js
+++ app/api/logout/route.js
@@ -1,24 +1,24 @@
-import { NextResponse, NextRequest } from 'next/server';
+// import { NextResponse, NextRequest } from 'next/server';
 
-export const GET = async (request: NextRequest) => {
-  try {
-    const token = request.cookies.get('token');
-    if (!token || token.value === '') {
-      return NextResponse.json(
-        { message: 'already logged out' },
-        { status: 400 }
-      );
-    }
+// export const GET = async (request: NextRequest) => {
+//   try {
+//     const token = request.cookies.get('token');
+//     if (!token || token.value === '') {
+//       return NextResponse.json(
+//         { message: 'already logged out' },
+//         { status: 400 }
+//       );
+//     }
 
-    const response = NextResponse.json({
-      message: 'logout Successfully',
-      success: true,
-    });
+//     const response = NextResponse.json({
+//       message: 'logout Successfully',
+//       success: true,
+//     });
 
-    response.cookies.set('token', '', { httpOnly: true });
+//     response.cookies.set('token', '', { httpOnly: true });
 
-    return response;
-  } catch (error: any) {
-    return NextResponse.json({ error: error.message }, { status: 500 });
-  }
-};
+//     return response;
+//   } catch (error: any) {
+//     return NextResponse.json({ error: error.message }, { status: 500 });
+//   }
+// };
