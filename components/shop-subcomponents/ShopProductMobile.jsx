ee03f0e2a5b7be54a4aae4869cc740da812a33b2
Mehmood Ul Hassan <mehmoodjutt471@gmail.com>
2024-09-11 23:00:41 +0500

    updated styling a bit

--- components/shop-subcomponents/ShopProductMobile.jsx
+++ components/shop-subcomponents/ShopProductMobile.jsx
@@ -6,24 +6,24 @@ import { AnimatePresence } from 'framer-motion'
 const ShopProductMobile = ({ addItem, products }) => {
     const [bgClicked, setBgClicked] = useState(false)
     return (
-        <AnimatePresence>
-            <div
-                className={`py-8 h-auto grid grid-cols-2 md:grid-cols-3 gap-6 scrollbar-hide lg:hidden`}
-                onClick={() => setBgClicked(true)}
-            >
-                {products.map((product, index) => (
-                    <MobileProductSlide
-                        key={index}
-                        product={product}
-                        addItem={addItem}
-                        vertical={false}
-                        bgClicked={bgClicked}
-                        setBgClicked={setBgClicked}
-                    />
-                ))}
+        // <AnimatePresence>
+        <div
+            className={`py-8 h-auto grid grid-cols-2 md:grid-cols-3 gap-6 scrollbar-hide lg:hidden`}
+            onClick={() => setBgClicked(true)}
+        >
+            {products.map((product, index) => (
+                <MobileProductSlide
+                    key={index}
+                    product={product}
+                    addItem={addItem}
+                    vertical={false}
+                    bgClicked={bgClicked}
+                    setBgClicked={setBgClicked}
+                />
+            ))}
 
-            </div>
-        </AnimatePresence>
+        </div>
+        // </AnimatePresence>
     )
 }
 
