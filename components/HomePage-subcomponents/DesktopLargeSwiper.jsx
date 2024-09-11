3756c7c26677c6f0362cf61cf01e1232663c3578
Mehmood Ul Hassan <mehmoodjutt471@gmail.com>
2024-09-11 16:13:11 +0500

    updated to backward

--- components/HomePage-subcomponents/DesktopLargeSwiper.jsx
+++ components/HomePage-subcomponents/DesktopLargeSwiper.jsx
@@ -19,7 +19,7 @@ const DesktopLargeSwiper = ({ onAddItem, handleNavigateDetails, data }) => {
         freeMode={true}
         direction="horizontal"
         modules={[FreeMode]}
-        spaceBetween={25}
+        // spaceBetween={25}
         className="mySwiper test ms-[30px]"
         onBeforeInit={(swiper) => {
           swiperRef.current = swiper;
@@ -64,8 +64,8 @@ const DesktopLargeSwiper = ({ onAddItem, handleNavigateDetails, data }) => {
         {data.map((product, index) => (
           <SwiperSlide key={product.id}>
             <div
-              className="slider-items"
-            // className="slider-items lg:ps-12 md:ps-8 sm:ps-4 ps-0"
+              // className="slider-items"
+              className="slider-items lg:ps-12 md:ps-8 sm:ps-4 ps-0"
             // style={
             //       index === 0 ? { paddingLeft: "50px", zIndex: '100' } : {}
             //     }
